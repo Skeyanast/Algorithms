1ee9f747419342bf06f5b78e7945a59fb9c1d17d
Skeyanast <125226458+Skeyanast@users.noreply.github.com>
2023-04-21 00:40:20 +0300

    Add files via upload

--- Tetrad1/Tetrad1_1.cpp
+++ Tetrad1/Tetrad1_1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -33,49 +34,6 @@ Node* addNode(int input, Node* top) {
 	return top;
 }
 
-void printTree(char method, Node* top) {
-	// Пользовательская функция
-	// Вход: символ выбора метода печати, название дерева
-	// Выход: выбранным способом печатается дерево
-	switch (method) {
-	case 'p':
-		if (top != nullptr) {
-			cout << top->value << " ";
-			printTree(method, top->left);
-			printTree(method, top->right);
-		}
-		break;
-	case 'o':
-		if (top != nullptr) {
-			printTree(method, top->left);
-			printTree(method, top->right);
-			cout << top->value << " ";
-		}
-		break;
-	case 's':
-		if (top != nullptr) {
-			printTree(method, top->left);
-			cout << top->value << " ";
-			printTree(method, top->right);
-		}
-		break;
-	default:
-		cout << "Введен неверный символ!" << endl;
-		break;
-	}
-}
-
-void delTree(Node* top) {
-	// Внутренняя функция
-	// Вход: название дерева
-	// Выход: удаление дерева
-	if (top != nullptr) {
-		if (top->left != nullptr) delTree(top->left);
-		if (top->right != nullptr) delTree(top->right);
-		delete top;
-	}
-}
-
 int depthTreeSys(int counter, Node* top) {
 	// Внутренняя функция
 	// Вход: 1, название дерева
@@ -105,6 +63,109 @@ int depthTree(Node* top) {
 	return depthTreeSys(counter, top);
 }
 
+int depthNodeSys(int input, int counter, Node* top)
+{
+	if (top != nullptr) {
+		if (input < top->value) {
+			counter++;
+			return depthNodeSys(input, counter, top->left);
+		}
+		else if (input > top->value) {
+			counter++;
+			return depthNodeSys(input, counter, top->right);
+		}
+		else return counter;
+	}
+	else return 0;
+}
+
+int depthNode(int input, Node* top)
+{
+	int counter = 1;
+	return depthNodeSys(input, counter, top);
+}
+
+void spaceprinter(int space)
+{
+	if (space > 1) {
+		for (int i = 1; i < space; i++) {
+			cout << "|   ";
+		}
+		cout << "|---";
+	}
+	else if (space == 1) {
+		cout << "|---";
+	}
+	else return;
+}
+
+void visualprintTreeSys(Node* top, int level)
+{
+	if (top != nullptr) {
+		if (level > 1) {
+			int space = depthNodeSys(top->value, level, top) - 1;
+			spaceprinter(space);
+		}
+		else {
+			int space = depthNode(top->value, top) - 1;
+			spaceprinter(space);
+		}
+		cout << top->value << "(" << top->count << ")" << endl;
+		level++;
+		visualprintTreeSys(top->left, level);
+		visualprintTreeSys(top->right, level);
+	}
+}
+
+void visualprintTree(Node* top)
+{
+	int level = 1;
+	visualprintTreeSys(top, level);
+}
+
+void printTree(char method, Node* top) {
+	// Пользовательская функция
+	// Вход: символ выбора метода печати, название дерева
+	// Выход: выбранным способом печатается дерево
+	switch (method) {
+	case 'p':
+		if (top != nullptr) {
+			cout << top->value << "(" << top->count << ")" << " ";
+			printTree(method, top->left);
+			printTree(method, top->right);
+		}
+		break;
+	case 'o':
+		if (top != nullptr) {
+			printTree(method, top->left);
+			printTree(method, top->right);
+			cout << top->value << "(" << top->count << ")" << " ";
+		}
+		break;
+	case 's':
+		if (top != nullptr) {
+			printTree(method, top->left);
+			cout << top->value << "(" << top->count << ")" << " ";
+			printTree(method, top->right);
+		}
+		break;
+	default:
+		cout << "Введен неверный символ!" << endl;
+		break;
+	}
+}
+
+void delTree(Node* top) {
+	// Внутренняя функция
+	// Вход: название дерева
+	// Выход: удаление дерева
+	if (top != nullptr) {
+		if (top->left != nullptr) delTree(top->left);
+		if (top->right != nullptr) delTree(top->right);
+		delete top;
+	}
+}
+
 bool searchNode(int input, Node* top) {
 	// Пользовательская функция
 	// Вход: значение искомого узла
@@ -121,159 +182,65 @@ bool searchNode(int input, Node* top) {
 	else return false;
 }
 
-Node* delNode(int input, Node* top) {
+Node* findMaximumKey(Node* ptr)
+{
+	while (ptr->right != nullptr) {
+		ptr = ptr->right;
+	}
+	return ptr;
+}
+
+void delNode(int input, Node* top) {
 	// Пользовательская функция
 	// Вход: значение удаляемого узла, название дерева
-	// Выход: 
+	// Выход: структура нового узла
 	if (top == nullptr) {
-		return top;
+		return;
 	}
-	if (input == top->value) {
-		Node* temp = new Node;
-		temp = nullptr;
-		if (top->right == nullptr) {
-			temp = top->left;
-		}
-		else {
-			Node* ptr = new Node;
-			ptr = top->right;
-			if (ptr->left == nullptr) {
-				ptr->left = top->left;
-				temp = ptr;
-			}
-			else {
-				Node* pmin = new Node;
-				pmin = ptr->left;
-				while (pmin->left != nullptr) {
-					ptr = pmin;
-					pmin = ptr->left;
-				}
-				ptr->left = pmin->right;
-				pmin->left = top->left;
-				pmin->right = top->right;
-				temp = pmin;
-			}
-		}
-		delete top;
-		return temp;
+
+	if (input < top->value) {
+		delNode(input, top->left);
 	}
-	else if (input < top->value) {
-		top->left = delNode(input, top->left);
+
+	else if (input > top->value) {
+		delNode(input, top->right);
 	}
+
 	else {
-		top->right = delNode(input, top->right);
-	}
-	return top;
-}
+		if (top->left == nullptr && top->right == nullptr) {
+			delete top;
+			top = nullptr;
+		}
 
-void WelcomeUser() {
-	// Внутренняя функция
-	// Вход: -
-	// Выход: приветствие пользователя
-	cout /*<< "Программа для работы с деревьями двоичного поиска"*/ << endl;
-	cout << "Доступные действия:" << endl;
-	cout << "1. Добавление элементов в дерево" << endl;
-	cout << "2. Вывод дерева" << endl;
-	cout << "3. Вычисление глубины дерева" << endl;
-	cout << "4. Поиск элемента в дереве" << endl;
-	cout << "5. Удаление элемента из дерева" << endl;
-	cout << "6. Удаление дерева и завершение программы" << endl << endl;
-	//cout << "Пустое дерево создано" << endl;
-}
+		else if (top->left != nullptr && top->right != nullptr) {
+			Node* predecessor = findMaximumKey(top->left);
+			top->value = predecessor->value;
+			top->count = predecessor->count;
+			delNode(predecessor->value, top->left);
+		}
 
-void FuncSelection(int choice, Node* tree) {
-	// Внутренняя функция
-	// Вход: номер функции, название дерева, с которым идет дальнейшая работа
-	// Выход: вывод результата выбранной функции
-	switch (choice) {
-	case 1:
-		cout << "Выбрано: Добавление элементов в дерево" << endl;
-		cout << "Сколько вы хотите добавить узлов? ";
-		int amount;
-		cin >> amount;
-		for (int i = 0; i < amount; i++) {
-			int temp;
-			cin >> temp;
-			tree = addNode(temp, tree);
-		}
-		cout << "Элементы успешно добавлены" << endl;
-		break;
-	case 2:
-		cout << "Выбрано: Вывод дерева" << endl;
-		cout << "Выберите метод печати ('p', 'o' или 's'): ";
-		char way;
-		cin >> way;
-		printTree(way, tree);
-		cout << endl;
-		break;
-	case 3:
-		cout << "Выбрано: Вычисление глубины дерева" << endl;
-		cout << "Глубина дерева = " << depthTree(tree) << endl;
-		break;
-	case 4:
-		cout << "Выбрано: Поиск элемента в дереве" << endl;
-		bool find;
-		cout << "Введите значение искомого узла: ";
-		int val;
-		cin >> val;
-		find = searchNode(val, tree);
-		if (find) {
-			cout << "Искомый узел найден" << endl;
-		}
 		else {
-			cout << "Искомый узел не найден" << endl;
+			Node* child = (top->left) ? top->left : top->right;
+			Node* curr = top;
+			top = child;
+			delete curr;
 		}
-		break;
-	case 5:
-		cout << "Выбрано: Удаление элемента из дерева" << endl;
-		bool exist;
-		cout << "Введите значение удаляемого узла: ";
-		int mass;
-		cin >> mass;
-		exist = searchNode(mass, tree);
-		if (exist) {
-			tree = delNode(mass, tree);
-			cout << "Узел успешно удален" << endl;
-		}
-		else {
-			cout << "Указанный узел не найден" << endl;
-		}
-		break;
-	case 6:
-		cout << "Выбрано: Удаление дерева и завершение программы" << endl;
-		delTree(tree);
-		cout << "Дерево удалено" << endl;
-		//cout << "Программа завершена." << endl;
-		break;
-	default:
-		cout << "Введена неверная команда!" << endl;
-		break;
 	}
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-		
+
 	Node* root = new Node;
 	root = nullptr;
-	cout << "Программа для работы с деревьями двоичного поиска" << endl;
-	cout << "Задайте значение корня дерева:" << endl;
-	int rtvl;
-	cin >> rtvl;
-	root = addNode(rtvl, root);
-	
-	WelcomeUser();
-	
-	while (true) {
-		int choice;
-		cout << endl << "Выберите действие" << endl;
-		cin >> choice;
-		FuncSelection(choice, root);
-		if (choice == 6) {
-			return 0;
-		}
+	root = addNode(5000, root);
+	for (int i = 27; i < 10000; i+=993) {
+		root = addNode(i / 10, root);
+		root = addNode(i % 10, root);
 	}
-	
+
+	visualprintTree(root);
+
 	return 0;
 }
